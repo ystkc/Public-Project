8ca219cf30ba328f1762fbc1a1e87e790dc0e9be
ystkc <1612162886@qq.com>
2020-07-11 12:25:24 +0800

    LatestUpdate20200711.1225

--- PPupdate.bat
+++ PPupdate.bat
@@ -1,4 +1,10 @@
+@echo off
 cd /d "E:\Git repository\Public-Project\"
 git add .
-git commit -m LatestUpdate%date:~0,4%%date:~5,2%%date:~8,2%.%time:~0,2%%time:~3,2%
+Set str=
+Set /p str=ÇëÌá¹©commit
+If not defined str (echo Ä¬ÈÏ£ºLatestUpdate%date:~0,4%%date:~5,2%%date:~8,2%.%time:~0,2%%time:~3,2%
+Set str=LatestUpdate%date:~0,4%%date:~5,2%%date:~8,2%.%time:~0,2%%time:~3,2%)else echo %str%
+
+git commit -m %str%
 git push --set-upstream PP master
--- timer_task/##README.txt
+++ timer_task/##README.txt
@@ -1,6 +1,6 @@
 timertask is a perishing , useless , and cumbersome multi-function TIMER!
 timertask是一个很垃圾的毫无用处的多功能计时器！
-版本信息
+版本信息 
 最新版(因为我也不记得是第几版了)
 ALREADY Latest version
 
