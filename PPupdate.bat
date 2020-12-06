663fd4a903f42f5e6d7bf2319b09080cfdc42457
dotoro <1612162886@github.com>
2020-12-06 10:04:42 +0800

    UpgradeTest

--- PPupdate.bat
+++ PPupdate.bat
@@ -6,8 +6,7 @@ Set /p str=ÇëÌá¹©commit
 If not defined str (echo Ä¬ÈÏ£ºLatestUpdate%date:~0,4%%date:~5,2%%date:~8,2%.%time:~0,2%%time:~3,2%
 Set str=LatestUpdate%date:~0,4%%date:~5,2%%date:~8,2%.%time:~0,2%%time:~3,2%)else echo %str%
 
+git status
 git commit -m %str%
 git push --set-upstream PP master
-git push --set-upstream PP clicking
-git push --set-upstream PP timer_task
 pause
